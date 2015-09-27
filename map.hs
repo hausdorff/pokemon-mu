d0fe5e81c80eb7480fd086644544217cc6e235c3
Alex Clemmer <clemmer.alexander@gmail.com>
2015-09-26 21:52:51 -0700

    Initial commit

---
+++ map.hs
@@ -0,0 +1,150 @@
+module Map where
+
+import Prelude hiding (Left, Right)
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Maybe as Maybe
+import qualified Data.Ord as Ord
+import qualified Data.Set as S
+import qualified Data.Vector as V
+
+--------------------
+-- Map data types --
+--------------------
+
+-- Get a point.
+-- Get one of the edges going in, and loop through the outgoing edges.
+-- For each of these, check:
+--   * if the outgoing edge is going back to the original point; if so, we should cat the ingoing edge with the self-loops on that node and the outgoing edge, and star it all. Then put that on the in-edge node.
+--   * if the outgoing edge is going to a different node, cat the in edge, the self-loops, and the out-edge. Put that on the in-edge node.
+
+type Grid = V.Vector (V.Vector Point)
+
+data Map = Map { points        :: Grid
+               , outgoingEdges :: EdgeMap
+               , incomingEdges :: EdgeMap
+               }
+instance Show Map where
+    show (Map rows _ _) = unlines $ V.toList (V.map convertRow rows)
+        where convertRow = map pointToChar . V.toList
+
+data Transition = Up | Down | Left | Right | A | B | Select | Start
+    deriving (Show)
+
+data Edge = Edge Point Point Transition
+
+type EdgeMap = M.Map Point [Edge]
+
+data MapSqr = Entrance | Exit | Wall | Grass | Ground
+    deriving (Eq, Ord)
+instance Show MapSqr where
+    show Entrance = "S"
+    show Exit     = "E"
+    show Wall     = "#"
+    show Ground   = " "
+instance Read MapSqr where
+    readsPrec _ "S" = [(Entrance, "")]
+    readsPrec _ "E" = [(Exit, "")]
+    readsPrec _ "#" = [(Wall, "")]
+    readsPrec _ " " = [(Ground, "")]
+
+data Point = Point Int Int MapSqr
+instance Eq Point where
+    (==) pt1@(Point x1 y1 sqr1) pt2@(Point x2 y2 sqr2) = xeq && yeq && sqreq
+        where xeq   = x1 == x2
+              yeq   = y1 == y2
+              sqreq = sqr1 == sqr2
+instance Ord Point where
+    compare pt1 pt2 = case (xcmp, ycmp, sqrcmp) of
+        (EQ, EQ, _) -> sqrcmp
+        (EQ, _, _)  -> ycmp
+        (_, _, _)   -> xcmp
+        where xcmp   = compare (x pt1) (x pt2)
+              ycmp   = compare (y pt1) (y pt2)
+              sqrcmp = compare (sqr pt1) (sqr pt2)
+instance Show Point where
+    show point = show $ sqr point
+
+x (Point x _ _) = x
+y (Point _ y _) = y
+sqr (Point _ _ sqr) = sqr
+
+-----------------------
+-- Utility functions --
+-----------------------
+
+makeOutgoingEdges :: Grid -> EdgeMap
+makeOutgoingEdges points = V.foldl processRow M.empty points
+    where processRow acc row     = V.foldl processPoint acc row
+          processPoint map point = M.insert point edges map
+              where edges = (neighborEdges points (x point) (y point))
+
+makeIncomingEdges :: Grid -> EdgeMap
+makeIncomingEdges points = M.empty
+ 
+getPoint :: Grid -> Int -> Int -> Maybe Point
+getPoint points x y = do
+    row <- points V.!? y
+    row V.!? x
+
+getEdge :: Grid -> Int -> Int -> Transition -> Maybe Edge
+getEdge points x y direction = do
+    from <- getPoint points x y
+    to <- getPoint points x' y'
+    makeEdge from to direction
+    where (x', y') = case direction of
+              Up    -> (x, y - 1)
+              Down  -> (x, y + 1)
+              Left  -> (x - 1, y)
+              Right -> (x + 1, y)
+
+makeEdge from to direction = case (sqr from, sqr to) of
+    (Wall, _) -> Nothing
+    (_, Wall) -> return $ Edge from from direction
+    otherwise -> return $ Edge from to direction
+
+neighborEdges :: Grid -> Int -> Int -> [Edge]
+neighborEdges points x y = Maybe.catMaybes neighbors
+    where directions = [Up, Down, Left, Right]
+          neighbors  = map (getEdge points x y) directions
+
+indexed :: [a] -> [(Int, a)]
+indexed xs = zip [0..] xs
+
+charToSqr :: Char -> MapSqr
+charToSqr c = read [c]
+
+sqrToChar :: MapSqr -> Char
+sqrToChar s = case (show s) of
+    [c] -> c
+
+pointToChar :: Point -> Char
+pointToChar p = case (show p) of
+    [c] -> c
+
+-----------------
+-- Graph utils --
+-----------------
+
+toGraph (Map points outgoingEdges _) = do
+    putStrLn "graph map {"
+    putStrLn "}"
+
+
+
+---------------
+-- Map tests --
+---------------
+
+makeMap rows = Map { points      = as2dVect
+                 , outgoingEdges = makeOutgoingEdges as2dVect
+                 , incomingEdges = makeIncomingEdges as2dVect
+                 }
+    where mkPt x y c           = Point x y (charToSqr c)
+          rowToPoints (y, row) = V.fromList $ map (\(x, c) -> mkPt x y c) (indexed row)
+          as2dVect             = V.fromList $ map rowToPoints (indexed rows)
+
+testM = makeMap ["####",
+               "# E#",
+               "#S #",
+               "####"]
