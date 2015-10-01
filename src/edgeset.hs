737e878981aa35280ccae0dccd693444cd885e53
Alex Clemmer <clemmer.alexander@gmail.com>
2015-09-30 19:19:20 -0700

    Add unifyAll and foldl to public API of edgeset.

--- src/edgeset.hs
+++ src/edgeset.hs
@@ -2,6 +2,8 @@ module Edgeset (
       Edgeset
     , empty
     , unify
+    , unifyAll
+    , Edgeset.foldl
     , fromList
     , toList
     ) where
@@ -34,6 +36,9 @@ unify set e@(Edge p1 p2 trans) = case (lookup key set) of
 unifyAll :: Foldable t => Edgeset -> t Edge -> Edgeset
 unifyAll set edges = L.foldl unify set edges
 
+foldl :: (a -> Edge -> a) -> a -> Edgeset -> a
+foldl f seed (Edgeset m) = M.foldl f seed m
+
 fromList :: [Edge] -> Edgeset
 fromList edges = L.foldl unify empty edges
 
@@ -47,7 +52,5 @@ toList set = Edgeset.foldl (\listAcc e -> e:listAcc) [] set
 lookup key (Edgeset m) = M.lookup key m
 
 insert key e (Edgeset m) = Edgeset (M.insert key e m)
-
-foldl f seed (Edgeset m) = M.foldl f seed m
 
 makeKey (Edge (Point x1 y1 _) (Point x2 y2 _) _) = ((x1, y1), (x2, y2))
