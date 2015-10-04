755ea7cb799516f77b62a8ca5703e75e866d2b81
Alex Clemmer <clemmer.alexander@gmail.com>
2015-10-03 20:48:42 -0700

    Add `unify` to edge.

--- src/edge.hs
+++ src/edge.hs
@@ -1,6 +1,7 @@
 module Edge (
       Edge(..)
     , makeEdge
+    , unify
     )where
 
 import MapSquare
@@ -9,9 +10,13 @@ import Transition
 
 data Edge = Edge Point Point Transition
 
+makeEdge :: Point -> Point -> Transition -> Maybe Edge
 makeEdge from to direction = case (sqr from, sqr to) of
     (Wall, _) -> Nothing
     (_, Wall) -> return $ Edge from from direction
     otherwise -> return $ Edge from to direction
+
+unify :: Edge -> Edge -> Edge
+unify (Edge p1 p2 trans) (Edge _ _ trans') = Edge p1 p2 (trans <|> trans')
 
 sqr (Point _ _ sqr) = sqr
